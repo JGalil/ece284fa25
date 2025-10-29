27d4e433d3662316b41bdbbd290185b766dfea8f
Joachim Galil <jgalil@ucsd.edu>
2025-10-29 14:50:27 -0700

    added functionality
    unsigned 4 bit * signed 4 bit + 16bit signed

--- hardware/wk3/prob3/verilog/mac.v
+++ hardware/wk3/prob3/verilog/mac.v
@@ -5,6 +5,21 @@ module mac (out, a, b, c);
 parameter bw = 4;
 parameter psum_bw = 16;
 
-...
+output [psum_bw-1:0] out;
+input  [bw-1:0] a;    // unsigned activation
+input  [bw-1:0] b;    // signed weight
+input  [psum_bw-1:0] c; // signed psum
+
+wire [2*bw-1:0] mult;
+
+wire [psum_bw-1:0] add;
+
+always_comb begin
+    mult = {1'b0, a} * {b[bw-1], b};
+    add = {{(psum_bw - 2*bw){mult[2*bw-1]}}, mult};
+    out = add + c;
+end
+
+
 
 endmodule
