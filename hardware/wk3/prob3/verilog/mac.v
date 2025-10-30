820c1489b38eb54f68c74d37365c5aebf43203c0
Joachim Galil <jgalil@ucsd.edu>
2025-10-29 22:52:19 -0700

    fix

--- hardware/wk3/prob3/verilog/mac.v
+++ hardware/wk3/prob3/verilog/mac.v
@@ -13,16 +13,14 @@ input  [psum_bw-1:0] c; // signed psum
 wire [bw-1:0] x0_us = x0, x1_us = x1, x2_us = x2, x3_us = x3;
 wire signed [bw-1:0] w0_s = w0, w1_s = w1, w2_s = w2, w3_s = w3;
 
-wire signed [2*bw-1:0] p0 = $signed(x0) * $signed({1'b0, w0});
+wire signed [2*bw-1:0] prod0 = $signed({1'b0, x0}) * $signed(w0);
+wire signed [2*bw-1:0] prod1 = $signed({1'b0, x1}) * $signed(w1);
+wire signed [2*bw-1:0] prod2 = $signed({1'b0, x2}) * $signed(w2);
+wire signed [2*bw-1:0] prod3 = $signed({1'b0, x3}) * $signed(w3);
+
 wire signed [2*bw-1:0] prod0 = p0[2*bw-1:0];
-
-wire signed [2*bw-1:0] p1 = $signed(x1) * $signed({1'b0, w1});
 wire signed [2*bw-1:0] prod1 = p1[2*bw-1:0];
-
-wire signed [2*bw-1:0] p2 = $signed(x2) * $signed({1'b0, w2});
 wire signed [2*bw-1:0] prod2 = p2[2*bw-1:0];
-
-wire signed [2*bw-1:0] p3 = $signed(x3) * $signed({1'b0, w3});
 wire signed [2*bw-1:0] prod3 = p3[2*bw-1:0];
 
 wire signed [2*bw:0] sum01 = prod0 + prod1;
