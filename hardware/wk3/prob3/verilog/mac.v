1ffec13ad5be2d82b06bdaff2ec843e7db8b32ed
Joachim Galil <jgalil@ucsd.edu>
2025-10-29 19:41:41 -0700

    update

--- hardware/wk3/prob3/verilog/mac.v
+++ hardware/wk3/prob3/verilog/mac.v
@@ -14,8 +14,8 @@ wire [2*bw:0] mult;
 
 wire [psum_bw-1:0] add;
 
-assign mult = {1'b0, a} * {{(bw){b[bw-1]}}, b};
-    assign add = {{(psum_bw - 2*bw+1){mult[2*bw]}}, mult};
-    assign out = add + c;
+assign mult = {1'b0, a} * {{b[bw-1]}, b};
+assign add = {{(psum_bw - (2*bw+1)){mult[2*bw]}}, mult};
+assign out = add + c;
 
 endmodule
