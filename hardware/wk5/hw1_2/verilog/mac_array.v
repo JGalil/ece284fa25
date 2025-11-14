80175dc8eec641542f4cb44d8409c5ac50ba3fb7
Joachim Galil <jgalil@ucsd.edu>
2025-11-13 17:24:19 -0800

    hardware coded up

--- hardware/wk5/hw1_2/verilog/mac_array.v
+++ hardware/wk5/hw1_2/verilog/mac_array.v
@@ -14,18 +14,39 @@ module mac_array (clk, reset, out_s, in_w, in_n, inst_w, valid);
   input  [psum_bw*col-1:0] in_n;
   output [col-1:0] valid;
 
+  wire [psum_bw*col*(row+1)-1:0] psum_temp;
+
+  wire [col*(row+1)-1:0] valid_temp;
+
+  wire [2*(row+1)-1:0] inst_temp;
+
+  assign psum_temp[psum_bw*col-1:0] = in_n;
+
   for (i=1; i < row+1 ; i=i+1) begin : row_num
       mac_row #(.bw(bw), .psum_bw(psum_bw)) mac_row_instance (
-      ...
-      ...
+        .clk(clk),
+        .reset(reset),
+        .in_w(in_w[bw*i-1:bw*(i-1)]),
+        inst_w(inst_w[2*i-1:2*(i-1)]),
+        .in_n(psum_temp[psum_bw*col*i-1: psum_bw*col*(i-1)]),
+        out_s(psum_temp[psum_bw*co*(i+1)-1:psum_bw*col*i]),
+        .valid(valid_temp[col*i-1:col*(i-1)])
       );
   end
 
+  assign out_s = psum_temp[psum_bw*col*(row+1)-1:psum_bw*col*row];
+  assign valid = valid_temp[col*(row+1)-1:col*row];
+
   always @ (posedge clk) begin
-
-
-   // inst_w flows to row0 to row7
- 
+    if(reset) begin
+      inst_temp <= 0;
+    end
+    else begin
+      inst_temp[1:0] <= inst_w;
+      for(int j = 1; j < row+1; j = j+1) begin
+        inst_temp[2*(j+1)-1:2*j] <= inst_temp[2*j-1:2*j];
+      end
+    end
   end
 
 
--- hardware/wk5/hw1_2/verilog/mac_tile.v
+++ hardware/wk5/hw1_2/verilog/mac_tile.v
@@ -7,24 +7,57 @@ parameter psum_bw = 16;
 
 output [psum_bw-1:0] out_s;
 input  [bw-1:0] in_w; // inst[1]:execute, inst[0]: kernel loading
-output [bw-1:0] out_e; 
+output [bw-1:0] out_e;
 input  [1:0] inst_w;
 output [1:0] inst_e;
 input  [psum_bw-1:0] in_n;
 input  clk;
 input  reset;
 
-...
-...
+wire [psum_bw-1:0] mac_out;
+
+reg [1:0] inst_q;
+reg [bw-1:0] a_q;
+reg [bw-1:0] b_q;
+reg [psum_bw-1:0] c_q;
+reg load_ready_q;
+
+assign out_e = a_q;
+assign inst_e = inst_q;
+assign out_s = mac_out;
 
 mac #(.bw(bw), .psum_bw(psum_bw)) mac_instance (
-        .a(a_q), 
+        .a(a_q),
         .b(b_q),
         .c(c_q),
-	.out(mac_out)
-); 
+        .out(mac_out)
+);
 
-...
-...
+always@(posedge clk)begin
+        if(reset) begin
+                inst_q <= 2'b0;
+                load_ready_q <= 1'b1;
+                a_q <= 0;
+                b_q <= 0;
+                c_q <= 0;
+        end
+        else begin
+                inst_q[1] <= inst_w[1];
+                if(inst_w[0] || inst_w[1]) begin
+                        a_q <= in_w;
+                end
+
+                if(inst_w[1] && load_ready_q) begin
+                        b_q <= in_w;
+                        load_ready_q <= 1'b0;
+                end
+
+                if(!load_ready_q) begin
+                        inst_q[0] <= inst_w[0];
+                end
+
+                c_q <= in_n;
+        end
+end
 
 endmodule
